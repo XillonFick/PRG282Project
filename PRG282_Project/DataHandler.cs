602f929ca8839939deef0e0f2586acbb148e5664
Xillon <60034172+Xillon@users.noreply.github.com>
2021-10-11 20:12:23 +0200

    Classes
    DataHandler
    FileHandler
    Student Class
    Main Form
    login Form

---
+++ PRG282_Project/DataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_Project
+{
+    class DataHandler
+    {
+        public DataHandler() { }
+        string connectionString = "Server=(local); Initial Catalog= ; Integrated Security = SSPI";
+        
+       
+    }
+}
---
+++ PRG282_Project/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_Project
+{
+    class Student
+    {
+        private string name;
+        private string surname;
+        private string course;
+        private DateTime doB;
+        private char gender;
+        private string phoneNumber;
+        private string address;
+        private string [] modulecodes;
+
+        public Student() { }
+
+        public Student(string name, string surname, string course, DateTime doB, char gender, string phoneNumber, string address, string  [] modulecodes)
+        {
+            this.Name = name;
+            this.Surname = surname;  
+            this.Course = course;
+            this.DoB = doB;
+            this.Gender = gender;
+            this.PhoneNumber = phoneNumber;
+            this.Address = address;
+            this.Modulecodes = modulecodes;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public string Course { get => course; set => course = value; }
+        public DateTime DoB { get => doB; set => doB = value; }
+        public char Gender { get => gender; set => gender = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Address { get => address; set => address = value; }
+        public string[] Modulecodes { get => modulecodes; set => modulecodes = value; }
+
+        public override string ToString()
+        {
+            string modules="";
+            foreach (var item in this.Modulecodes)
+            {
+                modules += item;
+            }
+            return string.Format("{0},{1},{2},{3},{4},{5},{6},{7}",
+                this.Name,this.Surname,this.Course,this.DoB,this.DoB.ToString(),this.Gender,this.PhoneNumber,this.Address,modules);
+        }
+    }
+}
